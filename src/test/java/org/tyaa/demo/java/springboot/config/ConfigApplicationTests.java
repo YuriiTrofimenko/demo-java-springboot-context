ef48071e68e132b49a0a6d1bad6995379b06f660
YuriiTrofimenko <tyaa@ukr.net>
2020-10-03 12:06:21 +0300

    init

---
+++ src/test/java/org/tyaa/demo/java/springboot/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.springboot.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
